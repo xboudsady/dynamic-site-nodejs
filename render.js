8e6fcef3fad02302473a661832aba2842ffb3950
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-01-16 19:59:53 -0800

    Added Render.js file

--- render.js
+++ render.js
@@ -1,10 +1,20 @@
 const fs = require('fs');
 
+function mergeValue(values, content) {
+    // Cycle over the keys
+    for (var key in values) {
+        // Replace all {{key}} with the value from the values object
+        content = content.replace("{{" + key + "}}", values[key]);
+    }
+    // return merged contents
+    return content;
+}
+
 function view(templateName, values, response) {
     // Read from the template file
-    const fileContents = fs.readFileSync('./views/' + templateName + '.html');
+    var fileContents = fs.readFileSync('./views/' + templateName + '.html');
     // Insert values into the contents
-
+    fileContents = mergeValues(values, fileContents);
     // Write out the contents to the response
     response.write(fileContents);
 }
